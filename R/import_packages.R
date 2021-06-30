89cc908c7e7c89b215d616e5004585455ba51dfa
esteful <estefaniacalparsoro@gmail.com>
2021-06-30 18:05:01 +0200

    heatmap

--- R/import_packages.R
+++ R/import_packages.R
@@ -2,12 +2,11 @@
 #'
 #' @import archdata
 #' @import devtools
-#' @import dplyr 
+#' @import dplyr
 #' @import ggplot2 ggfortify scales
 #' @import stargazer   rmarkdown
 #' @import ggthemes
-#' @import d3heatmap
-#' @import compositions 
+#' @import compositions
 #' @importFrom dendextend set labels_colors
 #' @importFrom devEMF emf
 #' @importFrom grDevices cm.colors dev.off heat.colors  palette pdf rainbow recordPlot replayPlot terrain.colors topo.colors

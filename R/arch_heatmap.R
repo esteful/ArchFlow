b13dab98d4262ebf9340256a334cb1fa09ff2cbe
esteful <estefaniacalparsoro@gmail.com>
2021-06-30 18:01:26 +0200

    disabling heatmap

--- R/arch_heatmap.R
+++ R/arch_heatmap.R
@@ -11,13 +11,13 @@
 
 
 
-"arch_heatmap" <- function(df_chem){
+#"arch_heatmap" <- function(df_chem){
 
   # Heatmap of the chemical composition ####
-    mat_div <- t(t(df_chem)/colMeans(df_chem))
+  #  mat_div <- t(t(df_chem)/colMeans(df_chem))
 
   #get the relative values of each composition
     #d3heatmap::d3heatmap(mat_div,       #colors are based on these values
-            cellnote=df_chem,       #show the raw chemical data
-            labRow= row.names(df_chem),width = 1050, height = 900)   #the names of the labels.
-}
+         #   cellnote=df_chem,       #show the raw chemical data
+          #  labRow= row.names(df_chem),width = 1050, height = 900)   #the names of the labels.
+#}
